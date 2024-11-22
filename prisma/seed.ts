0db3ad92dcc6b36da76922bdd03d0f8a37c86f3e
Petrik Diak <diak@petrik.hu>
2024-11-22 16:12:03 +0100

    dsa

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,36 @@
 import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker';
+import { connect } from 'http2';
 const prisma = new PrismaClient()
 async function main() {
  for(let i=0;i<50; i++){
   await prisma.steaming.create({
     data:{
-       cim:"cim"+i,
-       szero:"szero"+i,
-       Hossz:i,
-       ar:i
+      cim: faker.music.songName(),
+      szero: faker.music.artist(),
+      Hossz: faker.number.int({ min: 60, max: 150 }),
+      ar: faker.number.int({ min: 60, max: 150 }),
+      rate: faker.number.float({ min: 1, max: 5 })
 
     }
   })
  }
+ for(let i=0;i<10;i++){
+    await prisma.playlist.create({
+      data:{
+
+        name:faker.music.album(),
+        songs:{
+          connect:{
+            id:i+1
+          }
+        }
+      }
+      
+    })
+ }
+ 
+ 
 }
 main()
   .then(async () => {
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongModule } from './song/song.module';
+import { PlaylistModule } from './playlist/playlist.module';
 
 @Module({
-  imports: [SongModule],
+  imports: [SongModule, PlaylistModule],
   controllers: [AppController],
   providers: [AppService],
 })
